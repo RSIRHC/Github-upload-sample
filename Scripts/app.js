084ea32a38ad7ddcb35a057e441ba03be600b781
R.SIRHC <christopher.r99@live.com>
2016-05-17 20:40:12 -0400

    Initial Commit

---
+++ Scripts/app.js
@@ -0,0 +1,13 @@
+//Java Script goes here..
+
+//IIFE - Immediately Invoked Fuction Expression
+
+(function(){
+    "use strict";
+    
+    
+    //code goes in here..
+    
+    console.log("Portfolio started..")
+    
+})()
